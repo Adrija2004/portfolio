03412c47157d30562e76ab6021b48e419f1872cc
Adrija Banerjee <banarjeeadrija@gmail.com>
2025-07-31 20:58:40 +0530

    neww

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-
-window.onscroll = function() {
-  let topBtn = document.getElementById("topBtn");
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    topBtn.style.display = "block";
-  } else {
-    topBtn.style.display = "none";
-  }
-};
-
-
-document.getElementById("topBtn").onclick = function() {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-};
+document.querySelector('.logo').addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
